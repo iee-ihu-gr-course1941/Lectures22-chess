eb34b234c2f2d256f5278221d5245aa3c666dba2
Antonis Sidiropoulos <asidirop@gmail.com>
2022-11-15 21:01:23 +0200

    finish 15/11/22

--- www/chess.php
+++ www/chess.php
@@ -1,23 +1,23 @@
 <?php
 
+require_once "../lib/dbconnect.php"; 
+require_once "../lib/board.php";
+
 
 $method = $_SERVER['REQUEST_METHOD'];
-//  $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
-$request = explode('/', trim($_SERVER['SCRIPT_NAME'],'/'));
+$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+// $request = explode('/', trim($_SERVER['SCRIPT_NAME'],'/'));
 // Σε περίπτωση που τρέχουμε php –S 
 $input = json_decode(file_get_contents('php://input'),true);
-
-print_r($_SERVER);
-exit;
 
 switch ($r=array_shift($request)) {
     case 'board' : 
 	switch ($b=array_shift($request)) {
-		case '':
+		case '': 
 		case null: handle_board($method);break;
-		case 'piece': handle_piece($method, $request[0],$request[1],$input);
+		case 'piece': // handle_piece($method, $request[0],$request[1],$input);
 					break;
-		case 'player': handle_player($method, $request[0],$input);
+		case 'player': // handle_player($method, $request[0],$input);
 					break;
 		default: header("HTTP/1.1 404 Not Found");
 				break;
@@ -27,4 +27,16 @@ switch ($r=array_shift($request)) {
 	header("HTTP/1.1 404 Not Found");
 	exit;
 }
+
+function handle_board($method) {
+    if($method=='GET') {
+            show_board();
+    } else if ($method=='POST') {
+           reset_board();
+    } else {
+        header('HTTP/1.1405 Method Not Allowed');
+    }
+    
+}
+
 ?>

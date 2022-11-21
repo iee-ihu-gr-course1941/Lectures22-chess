9c8a9beb6d13f67f8e4e6b557c46558ac843a751
Antonis Sidiropoulos <asidirop@gmail.com>
2022-11-21 20:23:10 +0200

    finish chess1

--- www/chess.php
+++ www/chess.php
@@ -2,6 +2,7 @@
 
 require_once "../lib/dbconnect.php"; 
 require_once "../lib/board.php";
+require_once "../lib/game.php";
 
 
 $method = $_SERVER['REQUEST_METHOD'];
@@ -15,14 +16,20 @@ switch ($r=array_shift($request)) {
 	switch ($b=array_shift($request)) {
 		case '': 
 		case null: handle_board($method);break;
-		case 'piece': // handle_piece($method, $request[0],$request[1],$input);
+		case 'piece': handle_piece($method, $request[0],$request[1],$input);
 					break;
-		case 'player': // handle_player($method, $request[0],$input);
+		case 'player': handle_player($method, $request[0],$input);
 					break;
 		default: header("HTTP/1.1 404 Not Found");
 				break;
 	}
-	break;
+		break;
+	case 'status': 
+		if(sizeof($request)==0) {handle_status($method);}
+		else {header("HTTP/1.1 404 Not Found");}
+		break;
+	case 'players': handle_player($method, $request,$input);
+			break;
     default: 	
 	header("HTTP/1.1 404 Not Found");
 	exit;
@@ -34,9 +41,23 @@ function handle_board($method) {
     } else if ($method=='POST') {
            reset_board();
     } else {
-        header('HTTP/1.1405 Method Not Allowed');
+        header('HTTP/1.1 405 Method Not Allowed');
     }
     
 }
+function handle_piece($method, $x,$y,$input) {
+    ;
+}
 
+function handle_player($method, $p,$input) {
+    ;
+}
+
+function handle_status($method) {
+    if($method=='GET') {
+        show_status();
+    } else {
+        header('HTTP/1.1 405 Method Not Allowed');
+    }
+}
 ?>

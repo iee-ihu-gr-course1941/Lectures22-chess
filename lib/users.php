faf52fffba82c876a1ddf6b4f8d419fcfe147226
Antonis Sidiropoulos <asidirop@gmail.com>
2022-11-29 21:06:52 +0200

    chess3 do the move

--- lib/users.php
+++ lib/users.php
@@ -57,4 +57,19 @@ function set_user($b,$input) {
 	print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
 }
 
+
+function current_color($token) {
+	
+	global $mysqli;
+	if($token==null) {return(null);}
+	$sql = 'select * from players where token=?';
+	$st = $mysqli->prepare($sql);
+	$st->bind_param('s',$token);
+	$st->execute();
+	$res = $st->get_result();
+	if($row=$res->fetch_assoc()) {
+		return($row['piece_color']);
+	}
+	return(null);
+}
 ?>
